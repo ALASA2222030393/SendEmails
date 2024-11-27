c4fdfac7c2390a0326e99abbff01e33fad8be77e
Charaf <76937494+ALASA2222030393@users.noreply.github.com>
2024-11-27 16:39:07 +0100

    redy to update

--- last.js
+++ last.js
@@ -1,40 +1,20 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const htmlContent = require("./uniteck_email");
+const htmlContent = require("./formula");
 const app = express();
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', // Use service instead of host/port
+  service: 'gmail',
   auth: {
-    user: "unitechmanger@gmail.com", // Corrected 'user' key
-    pass: "aknk ywif rafj owcg", // Your app password
+    user: "YOUR_GOOGLE_EMAIL", 
+    pass: "YOUR_APP_PASSWORD", 
   },
 });
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.eu.mailgun.org",
-//   port: 465,
-//   secure: true,
-//     auth: {
-//       user: "brad@houssamboukhetou.net ", // Corrected 'user' key
-//       pass: "3ba2d4f024f51b7571a90d938d57b963-6df690bb-b40eb18e", // Your app password
-//     },
-//   });
 
+const emails = ["belhadjcharafeddine8@gmail.com"];
 
-const emails = [ 
-  "Khadidjabachouche18@gmail.com", "Khadidjabachouche18@gmail.com", 
-  "dorsafdorsaf074@gmail.com", "amanibenabderrahmane763@gmail.com", 
-  "amanibenabderrahmane763@gmail.com", "ayatelrrahmane892@gmail.com", 
-  "Kaimcharafeddine19@gmail.com", "shahrazedhamida@gmail.com", 
-  "mohamedzemieche8@gmail.com", "darine.ferrouh@gmail.com", "seifmemo2005@gmail.com",
-  "snaasma466@gmail.com", "snaasma466@gmail.com", "iyad98033@gmail.com", 
-  "briksaluha@gmail.com", "Hadilhoudhoddoudi@gmail.com", 
-  "Hadilhoudhoddoudi@gmail.com", "rahmarezaiki@gmail.com", 
-  "ikhlasbekhouche8@gmail.com", "assilguernah@gmail.com", 
-  "Sabaaamina137@gmail.com", "ltklinto@gmail.com"
-]
 
 app.get("/", async (req, res) => {
     try {
@@ -42,7 +22,7 @@ app.get("/", async (req, res) => {
       
         for (const email of emails) {
           const result = await transporter.sendMail({
-            from: '"unticheh" <achrafgt4@gmail.com>', // Use your actual email
+            from: '"unticheh" <achrafgt4@gmail.com>',
             to: email,
             subject: "Welcome to the workshop",
             text: "23-nov-2024",
